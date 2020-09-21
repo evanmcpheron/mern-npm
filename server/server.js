a135e86962689337be1278786d8ac20367166a53
Evan McPheron <evan.mcpheron@icloud.com>
2020-09-21 08:30:39 -0700

    Getting dotenv working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config({ path: '../.env' });
 
 const PORT = process.env.PORT || 5000;
+
+console.log(PORT);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
